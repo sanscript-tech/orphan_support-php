822e8c5f924cc27bcb2ae52e30172d1e970b1b17
Bhavesh Garg <54619203+bhavesh1129@users.noreply.github.com>
2020-11-08 23:24:40 +0530

    Update the title

--- etc/index.php
+++ etc/index.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Business Casual - Start Bootstrap Theme</title>
+    <title>Orphanage Support</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
