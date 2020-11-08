25bf840a07f350cd8ab262dc0bd6e63e458e6066
Bhavesh Garg <54619203+bhavesh1129@users.noreply.github.com>
2020-11-08 23:24:02 +0530

    Update the title

--- etc/contact.php
+++ etc/contact.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Contact - Business Casual - Start Bootstrap Theme</title>
+    <title>Orphanage Support</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
