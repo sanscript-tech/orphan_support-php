1596c00c80ceaa25bb7979968acc4cd69208d458
surajbokde <surajbokde890@gmail.com>
2020-11-19 19:02:45 +0530

    donation.sql

---
+++ donation.sql
@@ -0,0 +1,15 @@
+
+
+CREATE TABLE `donation` (
+  `id` int(255) NOT NULL,
+  `amount` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `donation`
+--
+
+INSERT INTO `donation` (`id`, `amount`) VALUES
+(1,500),(2,5000),(3,100),(4,170),(5,1200),(6,130);
+
+
